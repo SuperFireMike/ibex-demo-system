aabff41522a6e426b2cae750b8e39ae504d87b77
Michail Lolis <csd4673@csd.uoc.gr>
2025-08-01 17:13:02 +0300

    small change in resp_e declaration

--- rtl/system/axi_pkg.sv
+++ rtl/system/axi_pkg.sv
@@ -3,7 +3,10 @@ package axi_pkg;
     // Response type
     // EX0KAY IS NOT SUPPORTED
     typedef enum logic[1:0] { 
-        OKAY, EXOKAY, SLVERR, DECERR
+        OKAY = 2'b00, 
+        EXOKAY = 2'b01, 
+        SLVERR = 2'b10, 
+        DECERR = 2'b11
     } resp_e;
 
 endpackage : axi_pkg
